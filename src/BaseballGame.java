03bfd9a584a6405a027a58b1620692e969962850
inzulmi <inzulmi132@gmail.com>
2024-09-19 16:29:48 +0900

    클래스 메서드 수정

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -1,50 +1,107 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class BaseballGame {
-    // 게이머가 맞춰야 하는 대상
-    String target;
+    // 게임 난이도
+    private int difficulty;
+    // 게이머가 맞춰야 하는 목표
+    private String target;
+    // 게임 기록 List
+    private final List<Integer> scores = new ArrayList<>();
 
-    public BaseballGame(int difficulty) {
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setTarget() {
         // 1~9 까지의 숫자를 shuffle 을 이용해 섞음
         List<String> list = new ArrayList<>();
-        for(int i = 1; i < 10; i++) {
+        for(int i = 1; i < 10; i++)
             list.add(String.valueOf(i));
-        }
         Collections.shuffle(list);
-
         // 난이도에 알맞게 랜덤하게 섞인 1~9 중 특정 수 만큼 저장함.
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < difficulty; i++) {
+        for(int i = 0; i < this.difficulty; i++)
             sb.append(list.get(i));
-        }
         this.target = sb.toString();
     }
 
-    // 스트라이크 수를 세는 메서드
-    public String countstrike(String target) {
+    public String getTarget() {
+        return this.target;
+    }
+
+    public void play() {
+        Scanner sc = new Scanner(System.in);
         int count = 0;
-        for(int i = 0; i < this.target.length(); i++) {
-            if(this.target.charAt(i) == target.charAt(i)) {
+        while(true) {
+            try {
+                System.out.println("숫자를 입력하세요.");
+                String input = sc.nextLine();
+                // 조건에 맞는 예외 처리
+                validateInput(input);
+                // 결과를 String 으로 리턴 받음
+                String result = countStrike(input) + countBall(input);
+                // 시도 횟수 카운팅
                 count++;
+                // 모두 스트라이크면 모든 자리에 알맞은 수가 있는 것이므로 정답
+                if (result.equals(this.difficulty + "스트라이크 ")) {
+                    System.out.println("정답입니다!");
+                    // 전적 리스트에 시도 횟수 기록
+                    scores.add(count);
+                    break;
+                }
+                // 스트라이크와 볼이 없다면 아웃, 있다면 그대로 출력
+                if (result.isEmpty()) System.out.println("아웃");
+                else System.out.println(result);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                System.out.println();
             }
         }
+    }
+
+    void validateInput(String input) throws Exception {
+        // 조건에 맞는 예외 처리
+        if (!Pattern.matches("^[1-9]*$", input))
+            throw new Exception("1~9 사이의 숫자만 입력해 주세요.");
+        if (input.length() != this.difficulty)
+            throw new Exception(this.difficulty +"개의 숫자를 입력해주세요.");
+        for (int i = 0; i < this.difficulty; i++)
+            for (int j = i + 1; j < this.difficulty; j++)
+                if (input.charAt(i) == input.charAt(j))
+                    throw new Exception("중복된 숫자가 있습니다.");
+    }
+
+    public void printScores() {
+        if (!scores.isEmpty()) {
+            System.out.println("< 게임 기록 보기 >");
+            for (int i = 0; i < scores.size(); i++)
+                System.out.println((i + 1) + "번째 게임 : 시도 횟수 - " + scores.get(i));
+        } else System.out.println("게임 전적이 없습니다.");
+        System.out.println();
+    }
+    // 스트라이크 수를 세는 메서드
+    public String countStrike(String target) {
+        int count = 0;
+        for(int i = 0; i < this.target.length(); i++)
+            if(this.target.charAt(i) == target.charAt(i))
+                count++;
         // 스트라이크가 없으면 빈 String 리턴
         if(count == 0)  return "";
         return count + "스트라이크 ";
     }
     // 볼 수를 세는 메서드
-    public String countball(String target) {
+    public String countBall(String target) {
         int count = 0;
-        for(int i = 0; i < this.target.length(); i++) {
-            for(int j = 0; j < target.length(); j++) {
-                if (i == j) continue;
-                if(this.target.charAt(i) == target.charAt(j)) {
-                    count++;
-                }
-            }
-        }
+        for(int i = 0; i < this.target.length(); i++)
+            for(int j = 0; j < target.length(); j++)
+                if (i != j)
+                    if(this.target.charAt(i) == target.charAt(j))
+                        count++;
         // 볼이 없으면 빈 String 리턴
         if(count == 0) return "";
         return count + "볼";
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 게임 기록 저장용 List
-        List<Integer> list = new ArrayList<>();
+        BaseballGame baseballGame = new BaseballGame();
 
         System.out.print("환영합니다! ");
 // 쉬운 게임 종료를 위한 label
-label:
-        while(true) {
+label: while(true) {
             System.out.println("원하시는 번호를 입력하세요.");
             System.out.println("0. 자리수 설정 | 1. 게임 시작하기 | 2. 게임 기록 보기 | 3. 종료하기");
             String input = sc.nextLine();
-            // 0~3 만 입력 받고 예외 처리
-            try {
-                if(!Pattern.matches("[0-3]", input))
-                    throw new Exception("0~3 사이의 숫자만 입력해 주세요.\n");
-            } catch(Exception e) {
-                System.out.println(e.getMessage());
-                continue;
-            }
 
             // 난이도, 기본적으로는 3자리 게임
             int difficulty = 3;
+            // switch 를 이용
             switch (input) {
                 // 3 입력 시 break label 으로 전체 반복문 종료.
                 case "3": System.out.println("< 게임을 종료합니다. >"); break label;
                 // 2 입력 시 게임 전적 순서대로 출력
-                case "2":
-                    if (!list.isEmpty()) {
-                        System.out.println("< 게임 기록 보기 >");
-                        for (int i = 0; i < list.size(); i++)
-                            System.out.println((i + 1) + "번째 게임 : 시도 횟수 - " + list.get(i));
-                    } else System.out.println("게임 전적이 없습니다.");
-                    System.out.println();
-                    continue;
+                case "2": baseballGame.printScores(); break;
                 // 0 입력 시 난이도 재설정
                 case "0":
                     // 예외처리를 위한 반복문
@@ -55,48 +37,16 @@ label:
                             System.out.println(e.getMessage());
                         }
                     }
-            }
-
-            System.out.println("< 게임을 시작합니다. >");
-            // BaseballGame 생성자로 target 생성
-            BaseballGame baseballGame = new BaseballGame(difficulty);
-            // 시도 횟수 기록
-            int count = 0;
-            System.out.println(baseballGame.target);
-            while (true) {
-                try {
-                    System.out.println("숫자를 입력하세요.");
-                    input = sc.nextLine();
-                    // 조건에 맞는 예외 처리
-                    if (!Pattern.matches("^[1-9]*$", input))
-                        throw new Exception("1~9 사이의 숫자만 입력해 주세요.");
-                    if (input.length() != difficulty)
-                        throw new Exception("정해진 길이의 숫자만 입력해주세요.");
-                    for (int i = 0; i < input.length(); i++)
-                        for (int j = i + 1; j < input.length(); j++)
-                            if (input.charAt(i) == input.charAt(j))
-                                throw new Exception("중복된 숫자가 있습니다.");
-
-                    // 결과를 String 으로 리턴 받음
-                    String result = baseballGame.countstrike(input) + baseballGame.countball(input);
-                    // 시도 횟수 카운팅
-                    count++;
-                    // n 스트라이크면 모든 자리에 알맞은 수가 있는 것이므로 정답
-                    if (result.equals(difficulty + "스트라이크 ")) {
-                        System.out.println("정답입니다!");
-                        // 전적 리스트에 시도 횟수 기록
-                        list.add(count);
-                        break;
-                    }
-                    // 스트라이크와 볼이 없다면 아웃, 있다면 그대로 출력
-                    if (result.isEmpty()) System.out.println("아웃");
-                    else System.out.println(result);
-
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                } finally {
-                    System.out.println();
-                }
+                // break 없이 이어서 그대로 실행.
+                case "1":
+                    System.out.println("< 게임을 시작합니다. >");
+                    baseballGame.setDifficulty(difficulty);
+                    baseballGame.setTarget();
+                    System.out.println(baseballGame.getTarget());
+                    baseballGame.play();
+                    break;
+                // 0~3 이 아닌 입력은 무시.
+                default: System.out.println("0~3 사이의 숫자만 입력해 주세요.\n");
             }
         }
     }
